d8034521ab2ede22b873f2148f6e9dbf7cf270f2
Juuroku <chen92834020@gmail.com>
2023-02-18 08:11:31 +0800

    Add Comments

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 import sys
 import json
 
+# setup help message
 parser = argparse.ArgumentParser(add_help=True)
 
 parser.add_argument('-t', '--token',
@@ -26,8 +27,10 @@ parser.add_argument('-p', '--parse',
         help='Parse style of message. Markdown or HTML. Optional',
         type=str)
 
+# parse the arguments
 args = parser.parse_args()
 
+# check if parse mode is set
 if args.parse is not None:
     p = args.parse.lower()
     if p == 'markdownv2' or p == 'md' or p == 'markdown' :
@@ -37,8 +40,10 @@ if args.parse is not None:
     elif len(p) != 0 and p != 'default':
         sys.exit("Wrong Parse Mode: %s"%p)
 
+# build the url of the api
 url = "https://api.telegram.org/bot%s/sendMessage"%args.token
-obj = {}
+
+# build parameters of api
 obj = {}
 obj['chat_id'] = args.chat_id
 obj['text'] = args.message
@@ -46,8 +51,10 @@ obj['text'] = args.message
 if 'parse' in locals() or 'parse' in globals():
     obj['parse_mode'] = parse
 
+# send the request
 x = requests.post(url, json = obj)
 
+# get response from server
 result = json.loads(x.text)
 if not result['ok']:
     print("{} - {}".format(result['error_code'], result['description']))
